5245144fb64203c0e702d09d16a9bd37208d7fde
Kurbo1 <mcraftryan@gmail.com>
2022-10-22 08:54:15 -0400

    Small Auto Fix

--- src/main/java/frc/robot/AUTO/TwoBallAUTO.java
+++ src/main/java/frc/robot/AUTO/TwoBallAUTO.java
@@ -3,6 +3,7 @@ package frc.robot.AUTO;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.drive.DrivetrainSubsystem;
 import frc.robot.drive.Commands.FollowPath;
 import frc.robot.intake.IntakeSub;
@@ -10,6 +11,7 @@ import frc.robot.intake.Commands.IntakeBall;
 import frc.robot.sensors.LimelightSub;
 import frc.robot.shooter.ShooterSub;
 import frc.robot.shooter.Commands.AUTOShoot;
+import frc.robot.shooter.Commands.RunShooter;
 
 public class TwoBallAUTO extends SequentialCommandGroup {
 
@@ -18,9 +20,10 @@ public class TwoBallAUTO extends SequentialCommandGroup {
     public TwoBallAUTO(DrivetrainSubsystem drive, LimelightSub lime, ShooterSub shoot, IntakeSub in) {
         t = FollowPath.getTrajectoryFromPath("Start2Ball1.json");
         addCommands(
+            new FollowPath(t, drive).raceWith(new IntakeBall(in)),
             new AUTOShoot(drive, lime, shoot, in),
-            new FollowPath(t, drive)
-            .raceWith(new IntakeBall(in)));
+            new WaitCommand(2),
+            new RunShooter(shoot, () -> 0));
     }
 
     public Pose2d getInitialPose() {
