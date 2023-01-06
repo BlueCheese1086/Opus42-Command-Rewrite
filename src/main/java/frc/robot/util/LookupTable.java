680f59cabaafa6333e037ca44ec4326ca41d90c9
Kurbo1 <17462798+Kurbo1@users.noreply.github.com>
2023-01-06 11:34:12 -0500

    Balin frfr

---
+++ src/main/java/frc/robot/util/LookupTable.java
@@ -0,0 +1,14 @@
+package frc.robot.util;
+
+
+// Currently writing this to directly work with shooter
+// TODO: make this more universal later - we've got a pep rally to do
+public class LookupTable {
+    
+    private double[][] points;
+
+    public LookupTable(double[][] points) {
+        this.points = points;
+    }
+
+}
